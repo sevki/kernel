4022d2de892f4c2ad818ad4e1601bff0e1db21fe
Archie Pusaka <apusaka@chromium.org>
2021-08-27 23:26:52 +0800

    CHROMIUM: Disable Remote Wakeup for RTL8822 USB
    Some upstream patch enables RTL chips to remote wakeup in order
    to let future gens of RTK chips to act as wakeup source. However,
    it also enables RTL8822 to do so. Previously, RTL8822 remote wakeup
    capability are disabled in order to save power.
    
    This patch keeps remote wakeup for RTL8822 disabled in order to
    preserve the condition.
    
    BUG=b:178449733
    TEST=verify BT keyboard cannot wake Blooglet from suspend
    UPSTREAM-TASK=b:323789053
    
    Signed-off-by: Archie Pusaka <apusaka@chromium.org>
    
    Change-Id: I8bdf506da4c7c575ab69502d44afa246c423d827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5266181
    Reviewed-by: Yun-hao Chung <howardchung@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: Archie Pusaka <apusaka@chromium.org>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -330,6 +330,11 @@ enum {
 	 * during the hdev->setup vendor callback.
 	 */
 	HCI_QUIRK_BROKEN_LE_CODED,
+
+	/* CHROMIUM-only: This quirk prevents RTL8822 to perform remote wake
+	 * on system suspend to save power. This shouldn't be upstreamed.
+	 */
+	HCI_QUIRK_DISABLE_REMOTE_WAKE,
 };
 
 /* HCI device flags */
